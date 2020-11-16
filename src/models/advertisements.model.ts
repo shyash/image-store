40cbfa4bd68fe5d0113f7c8c6f1c9f21a08cb9a9
yash <shyash222@gmail.com>
2020-11-16 23:57:04 +0530

    update

--- src/models/advertisements.model.ts
+++
@@ -1,7 +0,0 @@
-import { model } from 'mongoose';
-import { AdvertisementDocument } from './advertisements.types';
-import { AdvertisementSchema } from './advertisements.schema';
-export const Advertisement = model<AdvertisementDocument>(
-	'Advertisement',
-	AdvertisementSchema,
-);
--- src/models/squares.model.ts
+++
@@ -1,4 +0,0 @@
-import { model } from 'mongoose';
-import { SquareDocument } from './squares.types';
-import { SquareSchema } from './squares.schema';
-export const Square = model<SquareDocument>('Square', SquareSchema);
